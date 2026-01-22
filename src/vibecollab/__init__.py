4c17333d34be74b1967c329cd0128d3828866d3b
ocarina <ocarina1024@gmail.com>
2026-01-22 13:40:17 +0800

    chore: bump version to 0.4.2

--- src/vibecollab/__init__.py
+++ src/vibecollab/__init__.py
@@ -7,7 +7,7 @@ Usage:
     vibecollab validate -c project.yaml
 """
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __author__ = "VibeCollab Contributors"
 
 from .generator import LLMContextGenerator
