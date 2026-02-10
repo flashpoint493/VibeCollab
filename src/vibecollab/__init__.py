ddb780b1a03a932daf4bdd909b5ab7c271a5e7d7
ocarina <ocarina1024@gmail.com>
2026-02-11 00:03:56 +0800

    feat: v0.5.2 增强多开发者协议检查和自动初始化

--- src/vibecollab/__init__.py
+++ src/vibecollab/__init__.py
@@ -7,7 +7,7 @@ Usage:
     vibecollab validate -c project.yaml
 """
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __author__ = "VibeCollab Contributors"
 
 from .generator import LLMContextGenerator
