b875afd9f83fa1f921b1408c8224fcf3748df371
ocarina <ocarina1024@gmail.com>
2026-02-24 14:11:45 +0800

    [FEAT] Add LLM Client - provider-agnostic API integration (v0.5.7)

--- src/vibecollab/__init__.py
+++ src/vibecollab/__init__.py
@@ -7,7 +7,7 @@ Usage:
     vibecollab validate -c project.yaml
 """
 
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 __author__ = "VibeCollab Contributors"
 
 from .generator import LLMContextGenerator
@@ -15,6 +15,7 @@ from .project import Project
 from .extension import ExtensionProcessor, Extension, Hook, Context
 from .event_log import Event, EventLog, EventType
 from .task_manager import Task, TaskManager, TaskStatus, ValidationResult
+from .llm_client import LLMClient, LLMConfig, LLMResponse, Message
 
 __all__ = [
     "LLMContextGenerator",
@@ -30,5 +31,9 @@ __all__ = [
     "TaskManager",
     "TaskStatus",
     "ValidationResult",
+    "LLMClient",
+    "LLMConfig",
+    "LLMResponse",
+    "Message",
     "__version__",
 ]
