6514e9e1804d85d8d44e442abdefe79c56100532
flashpoint493 <943107662@qq.com>
2026-02-27 01:55:03 +0800

    [RELEASE] v0.8.0 - 发布到 PyPI
    https://pypi.org/project/vibe-collab/0.8.0/

--- src/vibecollab/__init__.py
+++ src/vibecollab/__init__.py
@@ -7,7 +7,7 @@ Usage:
     vibecollab validate -c project.yaml
 """
 
-__version__ = "0.8.0.dev0"
+__version__ = "0.8.0"
 __author__ = "VibeCollab Contributors"
 
 from .event_log import Event, EventLog, EventType
