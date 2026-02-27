809bb275b1ab40a77ed5502f766e060adec6f9a8
flashpoint493 <943107662@qq.com>
2026-02-27 20:25:51 +0800

    release: v0.9.4 published to PyPI, bump to v0.10.0.dev0

--- src/vibecollab/__init__.py
+++ src/vibecollab/__init__.py
@@ -7,7 +7,7 @@ Usage:
     vibecollab validate -c project.yaml
 """
 
-__version__ = "0.9.4"
+__version__ = "0.10.0.dev0"
 __author__ = "VibeCollab Contributors"
 
 from .event_log import Event, EventLog, EventType
