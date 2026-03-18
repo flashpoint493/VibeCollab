8d1505662ce9e17cb43103a8e0bc87e6d98bc657
flashpoint493 <943107662@qq.com>
2026-03-18 17:10:30 +0800

    chore: cleanup temp commit script

--- scripts/_commit2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d d:\Github\VibeCollab
-git add -A
-git commit -m "docs: add DECISION-019, CHANGELOG v0.10.10, bump version" -m "- DECISION-019: v0.10.x strategic decisions (D1-D5)" -m "- CHANGELOG: v0.10.10 bug fixes + version sync tool" -m "- Version bump: 0.10.9 -> 0.10.10"
